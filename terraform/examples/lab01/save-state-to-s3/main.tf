7bd9e1120574bf3c7a120a880d79cdf0005da33a
rabanto <rabanto@gmail.com>
2024-03-25 03:20:25 +0000

    add new value

--- terraform/examples/lab01/save-state-to-s3/main.tf
+++ terraform/examples/lab01/save-state-to-s3/main.tf
@@ -3,11 +3,19 @@ terraform {
     bucket                  = "eflores-terraform-s3-state"
     key                     = "my-terraform-project"
     region                  = "us-east-1"
-    shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  profile = "s3-database-548850886359"
+}
+
+resource "aws_s3_bucket" "New_bucket" {
+  bucket = "my-personal-s3"
+  acl = "private"
+
+  tags = {
+    Name = "eflores"
+  }
 }
