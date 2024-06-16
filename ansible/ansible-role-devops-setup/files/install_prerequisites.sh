c6c3b1d5421f4bb58929315f61a307ed165a8522
2000923 <2000923@unmsm.edu.pe>
2024-06-15 20:18:59 -0500

    change the install_programs

--- ansible/ansible-role-devops-setup/files/install_prerequisites.sh
+++ ansible/ansible-role-devops-setup/files/install_prerequisites.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
-#
-# packages and install
-USER=$(whoami)
-SSH_HOME="/home/${USER}/.ssh"
-sudo apt update --force-yes -y
+sudo apt update --force-yes
 sudo apt install python3 python3-pip curl git ssh sshpass -y
-sleep 1m
 sudo pip3 install ansible-core
-band=false
-if [[ ! -d ${SSH_HOME} ]]; then
-  mkdir ~/.ssh && chmod 755 ~/.ssh
+# crear el directorio .ssh o las llaves private/public en caso de no tenerlas
+mkdir -p ~/.ssh ; if [[ ! -f ~/.ssh/id_rsa && ! -f ~/.ssh/id_rsa.pub ]]; then ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa; cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys ; fi
+# validar si tienes declarado el ^LAN en /etc/environment sino añadirlo.
+if ! grep -Ei "^(LAN|LC_ALL).*" /etc/environment; then
+    sudo sed -ie "1i LAN=en_US.utf-8" /etc/environment
+    sudo sed -ie "2i LC_ALL=en_US.utf-8" /etc/environment
+    sudo update-locale
 fi
-if [[ ! -f "/home/${USER}/.ssh/id_rsa" ]]; then
-  ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa
-fi
-cat ${SSH_HOME}/id_rsa.pub >> ~/.ssh/authorized_keys
+# configurar ansible.cfg para el usuario $(whoami)
+remote_user="$(whoami)"
+if [ ! -d ~/data ]; then mkdir ~/data ; fi 
+cat > ~/data/ansible.cfg<<EOF
+[defaults]
+host_key_checking = false
+remote_user = ${remote_user}
+private_key_file = /home/${remote_user}/.ssh/id_rsa
+roles_path = /home/${remote_user}/git.personal/programs/ansible/
+inventory = inventory
+callbacks_enabled = timer, profile_tasks, profile_roles
+pipelining = True
+timeout=30
+host_key_algorithms=ssh-rsa,ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521
+#%%%%%%%%%%%%%%%%%%%% LOGS %%%%%%%%%%%%%%%%%%%%%%
+log_path = /tmp/ansible_log_from_config.log
+#stdout_callback=default
+#display_skipped_hosts=no        ## To skip logging when task is skipped
+#display_ok_hosts=no
 
-if ! grep "^LAN=en_US.utf-8" /etc/environment; then
-  sudo sed -ie "1i LAN=en_US.utf-8" /etc/environment
-  band=true
-fi
-
-if ! grep "^LC_ALL=en_US.utf-8" /etc/environment; then
-  sudo sed -ie "2i LC_ALL=en_US.utf-8" /etc/environment
-  band=true
-fi
-
-if [[ band == true ]]; then 
-  sudo update-locale
-  sudo shutdown -r now
-fi
-
-# ansible-galaxy collection install community.general
-# ansible-galaxy collection install ansible.posix
-
+[privilege_escalation]
+become = true
+EOF
+export ANSIBLE_CONFIG="~/data/ansible.cfg"
+# AL finalizar de ejecutar el paso a paso o el script install_prerequisites.sh instalar los modulos de ansible-galaxy
+ansible-galaxy collection install community.general
+ansible-galaxy collection install community.vmware
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install ansible.windows
+if ! grep -o "alias vim" /etc/profile &>/dev/null; then echo 'alias vim="/snap/bin/nvim"'|sudo tee -a /etc/profile; fi
